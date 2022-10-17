e393fc4bd314c97f1a82441e8f3b6d19eb222508
Leo <leo.maedje@gmail.com>
2022-10-17 16:49:44 +0100

    Divided players into available and busy

--- server.ts
+++ server.ts
@@ -23,10 +23,21 @@ interface OnlinePlayer {
 }
 
 let onlinePlayers: OnlinePlayer[] = [];
+let busyPlayers: OnlinePlayer[] = [];
+
+function getPlayerById(playerArray: OnlinePlayer[], id: string): OnlinePlayer {
+  const player: OnlinePlayer = playerArray.filter(player => player.id === id)[0];
+  return player;
+}
+
+function removePlayerFromArray(playerArray: OnlinePlayer[], id: string): OnlinePlayer[] {
+  const alteredArray: OnlinePlayer[] = playerArray.filter(player => player.id !== id);
+  return alteredArray;
+}
 
 io.on("connection", (socket: Socket) => {
   console.log("connected", socket.id);
-  io.emit("players online updated", onlinePlayers);
+  io.emit("players online updated", onlinePlayers, busyPlayers);
     
   socket.on("reset", (opponent: OnlinePlayer) => {
     console.log("resetting");
@@ -34,8 +45,8 @@ io.on("connection", (socket: Socket) => {
   });
 
   socket.on("cell click", (changedBoard: Board, player: 'A'|'B', opponent: OnlinePlayer) => {
-    const lastPlayer: OnlinePlayer = onlinePlayers.filter(player => player.id === socket.id)[0];
-    console.log(`${lastPlayer.username} just moved as ${player}`, "next move by:", opponent.username);
+    const lastPlayer: OnlinePlayer = getPlayerById(onlinePlayers, socket.id);
+    console.log(`${lastPlayer.username} just moved as ${player},`, "next move by:", opponent.username);
     socket.to(opponent.id).emit("cell clicked by", changedBoard, player);
   })
 
@@ -44,29 +55,48 @@ io.on("connection", (socket: Socket) => {
     socket.to(opponent.id).emit("game won by", winner);
   })
 
-
   socket.on("new player online", (username: string) => {
     console.log(username, "now online");
     const newPlayer: OnlinePlayer = {username: username, id: socket.id};
     onlinePlayers.push(newPlayer);
-    io.emit("players online updated", onlinePlayers);
+    io.emit("players online updated", onlinePlayers, busyPlayers);
   })
 
-  socket.on("challenge", (invitedPlayerId: string, username: string) => {
-    socket.to(invitedPlayerId).emit("challenged", {username: username, id: socket.id});
+  socket.on("challenge", (invitedPlayerId: string) => {
+    const challenger: OnlinePlayer = getPlayerById(onlinePlayers, socket.id);
+    const busyPlayer: OnlinePlayer = getPlayerById(busyPlayers, invitedPlayerId);
+    console.log("busy player:", busyPlayer, (busyPlayer !== undefined));
+    if (busyPlayer !== undefined) {
+      socket.emit("player busy", busyPlayer);
+    } else {
+      socket.to(invitedPlayerId).emit("challenged", {username: challenger.username, id: challenger.id});
+    }
   })
 
-  socket.on("challenge accepted", (otherPlayerId) => {
-    socket.to(otherPlayerId).emit("your challenge accepted", socket.id);
+  socket.on("challenge accepted", (challengerId) => {
+    const opponent: OnlinePlayer = getPlayerById(onlinePlayers, socket.id);
+    const challenger: OnlinePlayer = getPlayerById(onlinePlayers, challengerId);
+    socket.to(challengerId).emit("your challenge accepted", opponent);
+    busyPlayers.push(opponent, challenger);
+    io.emit("players online updated", onlinePlayers, busyPlayers);
+  })
+
+  socket.on("left game", (opponent: OnlinePlayer) => {
+    const leavingPlayer: OnlinePlayer = getPlayerById(onlinePlayers, socket.id);
+    console.log(leavingPlayer.username, "left the game against", opponent.username);
+    socket.to(opponent.id).emit("opponent left game", leavingPlayer.username);
+    busyPlayers = removePlayerFromArray(busyPlayers, leavingPlayer.id);
+    busyPlayers = removePlayerFromArray(busyPlayers, opponent.id);
   })
 
   socket.on("disconnect", () => {
-    const offlinePlayer = onlinePlayers.filter(player => player.id === socket.id)[0];
+    const offlinePlayer: OnlinePlayer = getPlayerById(onlinePlayers, socket.id);
     if (offlinePlayer) {
       console.log(offlinePlayer.username, "is going offline");
     }
-    onlinePlayers = onlinePlayers.filter(player => player.id !== socket.id);
-    socket.broadcast.emit("players online updated", onlinePlayers);
+    busyPlayers = removePlayerFromArray(busyPlayers, socket.id);
+    onlinePlayers = removePlayerFromArray(onlinePlayers, socket.id);
+    socket.broadcast.emit("players online updated", onlinePlayers, busyPlayers);
   })
 
 });
